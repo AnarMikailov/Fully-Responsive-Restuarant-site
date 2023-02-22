48378ea59395c64752d327217be67f80ad2e948b
AnarMikailov <a.v.mikayilov@gmail.com>
2023-02-22 18:02:47 +0400

    video section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "./Intro.css";
+import { useRef } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import { useState } from "react";
 const Intro = () => {
-  return <div>Intro</div>;
+  const vidRef = useRef();
+  const [playVideo, setPlayVideo] = useState(false);
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+  return (
+    <div className="app__video">
+      <video
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+        src={meal}
+      ></video>
+      <div className="app__video-overlay flex__center">
+        <div className="app__video-overlay_circle" onClick={handleVideo}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -9,6 +9,42 @@ const Menu = () => {
         <SubHeading title="Menu that fits you palatte" />
         <h1 className="headtext__cormorant">Today's special</h1>
       </div>
+      <div className="app__specialMenu-menu">
+        <div className="app__specialMenu-menu_wine flex__center">
+          <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+          <div className="app__specialMenu_menu_items">
+            {data.wines.map((wine, index) => (
+              <MenuItem
+                key={wine.title + index}
+                title={wine.title}
+                tags={wine.tags}
+                price={wine.price}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="app__specialMenu-menu_img">
+          <img src={images.menu} alt="menu img" />
+        </div>
+        <div className="app__specialMenu-menu_cocktails flex__center">
+          <p className="app__specialMenu-menu_heading">Cocktails</p>
+          <div className="app__specialMenu_menu_items">
+            {data.cocktails.map((cocktail, index) => (
+              <MenuItem
+                key={cocktail.title + index}
+                title={cocktail.title}
+                tags={cocktail.tags}
+                price={cocktail.price}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div style={{ maginTop: "15px" }}>
+        <button type="button" className="custom__button">
+          View more
+        </button>
+      </div>
     </div>
   );
 };
